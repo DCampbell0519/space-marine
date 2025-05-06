c3146aff576b1d3252468537d409db9922cf7153
Donovan Campbell <donovan@dlcentertainment.net>
2025-05-05 21:00:19 -0700

    initial javascript code

--- js/app.js
+++ js/app.js
@@ -2,13 +2,28 @@
 
 
 /*---------- Variables (state) ---------*/
-let playerScore = 0;
+let playerScore;
 
 /*----- Cached Element References  -----*/
+const choiceElements = document.querySelectorAll('.square')
 
+const messageElement = document.querySelector('#message')
 
 /*-------------- Functions -------------*/
+function initialize() {
+    playerScore = 0;
+    // render();
+    // console.log("init 2")
+}
+window.onload = initialize;
+// console.log("init 1")
 
+function handleClick(choice) {
+    // console.log("choice test")
+}
 
 /*----------- Event Listeners ----------*/
+choiceElements.forEach(choice => {
+    choice.addEventListener('click', handleClick)
+})
 
