e86c94fc0ed69573a6ff4df2ad412bc98c374dca
Donovan Campbell <donovan@dlcentertainment.net>
2025-05-06 20:52:50 -0700

    adding new loading page and plot.js file

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ const messageElement = document.querySelector('#message')
 /*-------------- Functions -------------*/
 function initialize() {
     playerScore = 0;
+
     // render();
     // console.log("init 2")
 }
@@ -19,11 +20,16 @@ window.onload = initialize;
 // console.log("init 1")
 
 function handleClick(choice) {
-    // console.log("choice test")
+    console.log("choice test")
 }
 
 /*----------- Event Listeners ----------*/
 choiceElements.forEach(choice => {
     choice.addEventListener('click', handleClick)
+})
+
+document.getElementById('startButton').addEventListener('click', () => {
+    document.getElementById('gameContainer').style.display = 'none';
+    document.getElementById('adventureContainer').style.display = 'flex';
 })
 
